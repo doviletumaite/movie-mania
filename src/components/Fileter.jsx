923b62497fda3da7ebd124feb9f7693af59b87fd
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-02-21 22:13:00 +0100

    add genre

--- src/components/Fileter.jsx
+++ src/components/Fileter.jsx
@@ -17,6 +17,24 @@ const Filter = ({setActive, active, setFiltered, popular}) => {
           <button className={active === 0 ? "activeButton" : ""} onClick={()=> setActive(0)}>All</button>
           <button className={active === 35 ? "activeButton" : ""} onClick={()=> setActive(35)}>Comedy</button>
           <button className={active === 28 ? "activeButton" : ""} onClick={()=> setActive(28)}>Action</button>
+          <button className={active === 12 ? "activeButton" : ""} onClick={()=> setActive(12)}>Adventure</button>
+          <button className={active === 16 ? "activeButton" : ""} onClick={()=> setActive(28)}>Animation</button>
+          <button className={active === 80 ? "activeButton" : ""} onClick={()=> setActive(28)}>Crime</button>
+          <button className={active === 99 ? "activeButton" : ""} onClick={()=> setActive(28)}>Documentary</button>
+          <button className={active === 18 ? "activeButton" : ""} onClick={()=> setActive(28)}>Drama</button>
+          <button className={active === 10751 ? "activeButton" : ""} onClick={()=> setActive(10751)}>Family</button>
+          <button className={active === 14 ? "activeButton" : ""} onClick={()=> setActive(14)}>Fantasy</button>
+          <button className={active === 36 ? "activeButton" : ""} onClick={()=> setActive(36)}>History</button>
+          <button className={active === 27 ? "activeButton" : ""} onClick={()=> setActive(27)}>Horror</button>
+          <button className={active === 10402 ? "activeButton" : ""} onClick={()=> setActive(10402)}>Music</button>
+          <button className={active === 9648 ? "activeButton" : ""} onClick={()=> setActive(9648)}>Mystery</button>
+          <button className={active === 36 ? "activeButton" : ""} onClick={()=> setActive(36)}>History</button>
+          <button className={active === 10749 ? "activeButton" : ""} onClick={()=> setActive(10749)}>Romance</button>
+          <button className={active === 878 ? "activeButton" : ""} onClick={()=> setActive(878)}>Science Fiction</button>
+          <button className={active === 10770 ? "activeButton" : ""} onClick={()=> setActive(10770)}>TV Movie</button>
+          <button className={active === 53 ? "activeButton" : ""} onClick={()=> setActive(53)}>Thriller</button>
+          <button className={active === 10752 ? "activeButton" : ""} onClick={()=> setActive(10752)}>War</button>
+          <button className={active === 37 ? "activeButton" : ""} onClick={()=> setActive(37)}>Western</button>
       </div>
   )
 }
--- src/components/SingleMovie.jsx
+++ src/components/SingleMovie.jsx
@@ -1,11 +1,20 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-const SingleMovie = ({movie}) => {
-    return (
-     <motion.div layout animate={{  opacity:1 }} initial={{opacity: 0 }} exit={{opacity: 0}} className="movie-card">
-         <h3 className="movie-title">{movie.title}</h3>
-         <img className="movie-img" src={'https://image.tmdb.org/t/p/w500'+ movie.backdrop_path}/>
-     </motion.div>
-    )
-}
-export default SingleMovie
+const SingleMovie = ({ movie }) => {
+  return (
+    <motion.div
+      layout
+      animate={{ opacity: 1}}
+      initial={{ opacity: 0}}
+      exit={{ opacity: 0 }}
+      className="movie-card"
+    >
+      <h3 className="movie-title">{movie.title}</h3>
+      <img
+        className="movie-img"
+        src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
+      />
+    </motion.div>
+  );
+};
+export default SingleMovie;
