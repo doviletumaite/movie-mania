d1802c68b1548c952e01f3feb00a48d2021b1faf
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-02-21 17:23:18 +0100

    up

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react"
+import Filter from "./components/Fileter.jsx"
 import SingleMovie from "./components/SingleMovie.jsx"
 import "./style/generalStyle.css"
 
 const HomePage = () => {
     const [popularMovies, setPopularMovies] = useState([])
     const fetchMovies = async () => {
-        const moviesToFetch  = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e6137a25c112d7a3931970b2d03b3b4&language=en-US&page=2")
+        const moviesToFetch  = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=4e6137a25c112d7a3931970b2d03b3b4&language=en-US&page=1")
         const movies = await moviesToFetch.json()
         setPopularMovies(movies.results)
     }
@@ -15,7 +16,9 @@ const HomePage = () => {
   console.log("movies", popularMovies)
     return (
         <>
+        <Filter/>
         <div className="movie-board">
+            
             {
                 popularMovies.map(m => 
                     { return <SingleMovie key={m.id} movie={m}/>})
---
+++ src/components/Fileter.jsx
@@ -0,0 +1,12 @@
+import "../style/filterStyle.css"
+
+const Filter = () => {
+  return (
+      <div className="filter-container">
+          <button>All</button>
+          <button>Comedy</button>
+          <button>Action</button>
+      </div>
+  )
+}
+export default Filter
