6d0c63271376f18bdb71353e1eae4be0fd0f7e7e
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-02-21 17:55:07 +0100

    filter

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,10 +5,13 @@ import "./style/generalStyle.css"
 
 const HomePage = () => {
     const [popularMovies, setPopularMovies] = useState([])
+    const [filtered, setFiltered] = useState([])
+    const [active, setActive] = useState(0)
     const fetchMovies = async () => {
         const moviesToFetch  = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=4e6137a25c112d7a3931970b2d03b3b4&language=en-US&page=1")
         const movies = await moviesToFetch.json()
         setPopularMovies(movies.results)
+        setFiltered(movies.results)
     }
    useEffect(()=> {
     fetchMovies()
@@ -16,11 +19,11 @@ const HomePage = () => {
   console.log("movies", popularMovies)
     return (
         <>
-        <Filter/>
+        <Filter popular={popularMovies} setFiltered={setFiltered} active={active} setActive={setActive}/>
         <div className="movie-board">
             
             {
-                popularMovies.map(m => 
+                filtered.map(m => 
                     { return <SingleMovie key={m.id} movie={m}/>})
             }
         </div>
--- src/components/Fileter.jsx
+++ src/components/Fileter.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react"
 import "../style/filterStyle.css"
 
-const Filter = () => {
+const Filter = ({setActive, active, setFiltered, popular}) => {
+    useEffect(()=> {
+      if(active === 0) {
+          setFiltered(popular)
+          return
+      }
+      const filteredMovies = popular.filter((m) => m.genre_ids.includes(active))
+      setFiltered(filteredMovies)
+    },[active])
+    
   return (
       <div className="filter-container">
-          <button>All</button>
-          <button>Comedy</button>
-          <button>Action</button>
+          <button onClick={()=> setActive(0)}>All</button>
+          <button onClick={()=> setActive(35)}>Comedy</button>
+          <button onClick={()=> setActive(28)}>Action</button>
       </div>
   )
 }
